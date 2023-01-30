4d8fc7549593d4e47a210f6a23922dd3d55d3f29
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2023-01-30 15:06:26 +0600

    Text widget limitation

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,37 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Expense App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      title : 'Flutter App',
+      home : MyHomePage(),
+    );
+}
+}
+
+class MyHomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar : AppBar(
+        title: Text("Expense App"),
+        ) ,
+      body : Column(
+        children: <Widget>[
+          Card(
+            //here card size will depends on Text widget, so we can not increase size
+            child : Text("Upper Portion"),
+            elevation : 5,
+          ),
+          Card(
+            child : Text("Lower Portion"),
+            elevation : 5,
+
+          ),
+        ],
       ),
-      home: Text('Expense APP Home Page'),
     );
   }
 }
